ac7194fbca60abe3ea5c8928afa101bb3c07dc60
DKirbi <davor.kirbis@gmail.com>
2023-01-03 11:59:27 +0100

    added a few examples of different javascript types

--- scripts/app.js
+++ scripts/app.js
@@ -52,3 +52,54 @@ const percentage = actual / max;
 console.log(percentage);
 const percentagePrint = Math.round(percentage*100) + '%';
 console.log(percentagePrint);
+
+//Strings
+//Numbers are always typed in directly, without using any quotes. Strings however can be typed in three different ways
+
+let str = "hello";
+let str2 = 'world';
+let phrase = `We can put two strings together ${str} ${str2}`;
+console.log(phrase);
+
+
+//Backtics are "extended functionality" in javascript, they help us to embed variables and expressions using ${} into a string
+
+let MyName = 'Davor';
+console.log(`Hello, ${MyName}`);
+
+console.log(`the result is ${1+3}`);
+
+//True False expressions
+
+let isGreater = 4 > 1;
+console.log(isGreater);
+//the variable will return 'true' in the conosole
+
+//undefined and null are two 'special not really special' types in Javascript. Null is an empty value while Undefined, is a variable that has no value or has not been defined
+
+let newAge = 100;
+newAge = undefined;
+
+console.log(newAge);
+
+//typeof operator defines the type of the operand
+
+const notDefined = typeof undefined; //'undefined'
+const zero = typeof 0; // 'number'
+const bigInteger = typeof 10n; // big integer
+const booLean = typeof true; //'boolean'
+const words = typeof "foo"; //string
+const sYmbol = typeof Symbol("id"); // "symbol"
+const mathematics = typeof Math; // object (1)
+const nothing = typeof null // object (2)
+const alarm = typeof alert // function (3)
+
+console.log(notDefined);
+console.log(zero);
+console.log(bigInteger);
+console.log(booLean);
+console.log(words);
+console.log(sYmbol);
+console.log(mathematics);
+console.log(nothing);
+console.log(alarm);
